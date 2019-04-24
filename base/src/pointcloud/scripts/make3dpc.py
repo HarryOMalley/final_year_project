f6beb552bc4a0122696aab1efe0bb2096821f911
Harry O'Malley <hazaomalley@gmail.com>
2019-04-24 17:42:05 +0100

    Modified Script
    Changed to use just data rather than data.data (which was an attribute that LaserScan data does not have)

--- base/src/pointcloud/scripts/make3dpc.py
+++ base/src/pointcloud/scripts/make3dpc.py
@@ -4,7 +4,7 @@ from std_msgs.msg import String
 from sensor_msgs.msg import LaserScan
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
 
 
 def listener():
